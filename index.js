50de8bcfe74b4d819946465ca459d6e48c292f1d
EmekaAgara <emekaagara@gmail.com>
2022-06-21 17:05:29 +0100

    for loop

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
@@ -6,4 +6,8 @@ inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
 
